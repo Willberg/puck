f72bc992a836c85856ee457ba7674d6f6678af8d
Willberg <hszhongtao@163.com>
2020-03-30 19:44:10 +0800

    双检查单例模式

--- src/pattern/Singleton.java
+++ src/pattern/Singleton.java
@@ -2,10 +2,21 @@ package pattern;
 
 // 饿汉式，懒汉式，双检查单例模式（线程安全）
 public class Singleton {
-    private volatile static int value;
+    private volatile static Singleton instance;
 
     //双检查单例模式（线程安全）
-    public void instance() {
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
 
+    public static void main(String[] args) {
+        Singleton.getInstance();
     }
 }
