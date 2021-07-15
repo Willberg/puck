5f4d620eded8098efd3a4136f7e5e22cd2b4f1cd
bill <hszhongtao@163.com>
2021-07-15 23:39:49 +0800

    多态

--- src/jdk/Son.java
+++ src/jdk/Son.java
@@ -5,11 +5,16 @@ public class Son extends Father {
 		System.out.println("this is son");
 	}
 
+	public void onlySon() {
+		System.out.println("OnlySon");
+	}
+
 	public static void main(String[] args) {
 		Father f = new Son();
 		f.print();
 		f.test();
 		f.f();
+		((Son) f).onlySon();
 
 		f = new Father();
 		f.print();
