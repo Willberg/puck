02d95e57531f3728a5e540b3467724bf9bae6d02
bill <hszhongtao@163.com>
2021-09-28 01:07:48 +0800

    排序

--- src/algorithm/mine/sort/MySort.java
+++ src/algorithm/mine/sort/MySort.java
@@ -1,13 +1,15 @@
 package algorithm.mine.sort;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MySort {
     private int[] arr;
     private int[] oriArr;
 
-    public MySort(int n, int rand) {
-        this.oriArr = generateRandArray(n, rand);
+    public MySort(int m, int rand, int n) {
+        this.oriArr = generateRandArray(m, rand, n);
         this.arr = deepCopy(this.oriArr);
     }
 
@@ -208,14 +210,94 @@ public class MySort {
         }
     }
 
-    // 桶排序
+    /**
+     * 桶排序
+     * 确定桶的数量（max-min）/length + 1，将数据分布到各个桶中，分别对桶中元素排序，最后将元素复制到原数组中，适合最大值和最小值相差大的数组排序
+     */
     public void bucketSort() {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        List<Integer> arrList = new ArrayList<>(arr.length);
+        for (int v : arr) {
+            arrList.add(v);
+            max = Math.max(max, v);
+            min = Math.min(min, v);
+        }
 
+        int bucketNum = (max - min) / arr.length + 1;
+        List<List<Integer>> buckets = new ArrayList<>(bucketNum);
+        for (int i = 0; i < bucketNum; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for (int v : arr) {
+            int idx = (v - min) / arr.length;
+            buckets.get(idx).add(v);
+        }
+
+        // 对桶内元素进行排序
+        int j = 0;
+        for (int i = 0; i < buckets.size(); i++) {
+            List<Integer> list = buckets.get(i);
+            if (list != null) {
+                insertSort(list);
+                for (int v : list) {
+                    // 将桶内元素放回原数组
+                    arr[j++] = v;
+                }
+            }
+        }
     }
 
-    // 基数排序
-    public void radixSort() {
+    private void insertSort(List<Integer> list) {
+        if (list == null) {
+            return;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i; j > 0; j--) {
+                if (list.get(j) < list.get(j - 1)) {
+                    swap(list, j - 1, j);
+                }
+            }
+        }
+    }
 
+    private void swap(List<Integer> list, int i, int j) {
+        int tmp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, tmp);
+    }
+
+    /**
+     * 基数排序
+     * 获取最大位数的位数，从低位到高位依次取每个位作为基数数组，进行计数排序(适用于非负数)
+     */
+    public void radixSort() {
+        int max = Integer.MIN_VALUE;
+        for (int v : arr) {
+            max = Math.max(v, max);
+        }
+
+        List<List<Integer>> list = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        int dev = 1;
+        int mod = 10;
+        for (int i = max; i > 0; i /= 10, dev *= 10) {
+            for (int v : arr) {
+                int idx = (v / dev) % mod;
+                list.get(idx).add(v);
+            }
+
+            int pos = 0;
+            for (int j = 0; j < list.size(); j++) {
+                for (int k = 0; k < list.get(j).size(); k++) {
+                    arr[pos++] = list.get(j).get(k);
+                }
+            }
+        }
     }
 
     public void swap(int i, int j) {
@@ -246,13 +328,22 @@ public class MySort {
         return retArr;
     }
 
-    public int[] generateRandArray(int n, int rand) {
-        int[] retArr = new int[n];
+    /**
+     * 生成随机数
+     *
+     * @param m    随机数个数
+     * @param rand 随机数范围
+     * @param n    随机数正数比例
+     * @return 随机数数组
+     */
+    public int[] generateRandArray(int m, int rand, int n) {
+        int[] retArr = new int[m];
+        n = Math.abs(n);
         Random random = new Random();
         int i = 0;
-        while (i < n) {
+        while (i < m) {
             // 随机符号
-            int sign = random.nextInt(rand) % 2 == 0 ? 1 : -1;
+            int sign = random.nextInt(rand) % n == 0 ? 1 : -1;
             retArr[i++] = random.nextInt(rand) * sign;
         }
 
@@ -260,8 +351,8 @@ public class MySort {
     }
 
     public static void main(String[] args) {
-        MySort mySort = new MySort(30, 10);
-//        int[] arr = {1, 6, 2, 0, 7, 9, 20, 11, 24, 6, 2, 1, 4542, 5};
+        int m = 20, rand = 10, n = 2;
+        MySort mySort = new MySort(m, rand, n);
         mySort.printArr(true);
 
         int[] arr = mySort.getOriArr();
@@ -297,5 +388,14 @@ public class MySort {
         mySort.countSort();
         mySort.printArr(false);
 
+        mySort.setArr(arr);
+        mySort.bucketSort();
+        mySort.printArr(false);
+
+        int[] radixArr = mySort.generateRandArray(m, rand, 1);
+        mySort.setArr(radixArr);
+        mySort.radixSort();
+        mySort.printArr(true);
+        mySort.printArr(false);
     }
 }
