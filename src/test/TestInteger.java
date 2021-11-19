b92742f9569c53284fda09e802e83839945026bf
Willberg <hszhongtao@163.com>
2021-11-19 23:45:01 +0800

    整形数测试

---
+++ src/test/TestInteger.java
@@ -0,0 +1,17 @@
+package test;
+
+public class TestInteger {
+    public static void main(String[] args) {
+        Integer a = -128;
+        Integer b = -128;
+        System.out.println(a==b);
+
+        Integer c = 128;
+        Integer d = 128;
+        System.out.println(c==d);
+
+        Integer e = Integer.valueOf(-128);
+        Integer f = Integer.valueOf(-128);
+        System.out.println(e==f);
+    }
+}
